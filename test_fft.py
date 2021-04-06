fbd7655748ae05352f23c7fffd593c1e8924b676
Gabrielle Germain <57801267+GAGER41@users.noreply.github.com>
2021-04-06 15:01:23 -0400

    fourier

--- test_fft.py
+++ test_fft.py
@@ -1,4 +1,16 @@
 import scipy
-import numpy as numpy
-from mat import pyplot as pyplot
+import numpy as np
+from matplotlib import pyplot as plt
 
+position = r'C:\Users\gabri\Documents\Université\Session 4\Optique (lab)\spectroscopie ir\test_lum_blanche.txt'
+#data = np.array(pd.read_csv(position, usecols=[3, 4])).transpose()
+data = np.loadtxt(position, delimiter="	", skiprows=18, usecols=(1, 2)).transpose()
+
+position = data[0]
+_signal = data[1]
+fourier = np.fft.fft(_signal)
+#plt.plot(position, _signal)
+plt.plot(position, fourier)
+plt.show()
+
+# Donc, la fonction np.fft.fft permet de faire des transformées de fourier sur une matrice de data
