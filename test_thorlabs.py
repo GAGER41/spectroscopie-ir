f7ff8844efb61d8e77e5e43b3698585f41d891cf
Gabrielle Germain <57801267+GAGER41@users.noreply.github.com>
2021-04-13 15:18:28 -0400

    fourier

--- test_thorlabs.py
+++ test_thorlabs.py
@@ -4,15 +4,30 @@ from matplotlib import pyplot as plt
 import pandas as pd
 
 position = r'C:\Users\gabri\Documents\Université\Session 4\Optique (lab)\spectroscopie ir\lumiere_blanche_thorlabs2.csv'
-#data_j = np.loadtxt(position_j, delimiter="	", skiprows=18, usecols=(1, 2)).transpose()
-data = pd.read_csv(position)
-#data = np.loadtxt(position, delimiter="    ")
-data = np.array(data).transpose()
-print(data.size)
-temps = data[0]
-_signal = data[1]
+data = pd.read_csv(position)                # lecture du csv
+data = np.array(data).transpose()           # conversion du data en array
 
-#print(data)
 
-plt.plot(temps/1000, _signal)
+## On ne connait pas le déplacement, mais les acquisitions sont faites sur 2 minutes (120 secondes)
+## Par contre, on connait le temps entre les acquisitions et le pas, donc 1 micros-pas = 0.1 micron, à chaque 80 ms
+## Vitesse de déplacement = 0.1micron/80ms, donc on peut connaître le déplacement total lors de l'acquisition
+
+vitesse = 0.1e-6/80e-3
+distance = 120 * vitesse
+
+temps = data[0]                                                     # temps
+position = np.linspace(0, distance, len(temps))                     # matrice position généré avec le temps et la vitesse
+_signal = data[1] - np.average(data[1])                             # signal, puissance
+
+fourier = np.fft.fft(_signal)                                       # fft du signal
+freq = np.fft.fftfreq(len(position), d=position[1]-position[0])     # on génère une matrice des fréquences dans l'espace de Fourier
+
+# on garde seulement la 2e moitié qui est positive
+
+lo_pos = 3e8 / freq[:int(len(freq)/2)]                              # *3e8*1e6
+fourier_pos = abs(fourier[:int(len(fourier)/2)])
+
+#plt.plot(freq[:int(len(freq)/2)]*3e8*1e6, abs(fourier[:int(len(fourier)/2)]))           #on plot seulement la deuxième moitié, pas les fréquences négatives.
+plt.plot(lo_pos[:400], fourier_pos[:400])
 plt.show()
+
