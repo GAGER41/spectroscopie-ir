665cd3b98dba33fac7ec459c4200158f345ed468
Sandrine Poulin <sandrine.poulin.3@ulaval.ca>
2021-04-19 17:37:43 -0400

    Dernier commit i swear
    or else imma kermitt

--- test_thorlabs.py
+++ test_thorlabs.py
@@ -12,12 +12,12 @@ data = np.array(data).transpose()           # conversion du data en array
 ## Par contre, on connait le temps entre les acquisitions et le pas, donc 1 micros-pas = 0.1 micron, à chaque 80 ms
 ## Vitesse de déplacement = 0.1micron/80ms, donc on peut connaître le déplacement total lors de l'acquisition
 
-vitesse = 0.1e-6/80e-3
-distance = 120 * vitesse
+vitesse = 0.1e-6/80e-3 #m/s
+distance = 120 * vitesse #m
 
-temps = data[0]                                                     # temps
-position = np.linspace(0, distance, len(temps))                     # matrice position généré avec le temps et la vitesse
-_signal = data[1] - np.average(data[1])                             # signal, puissance
+temps = data[0] #s                                                     # temps
+position = np.linspace(0, distance, len(temps))                        # matrice position généré avec le temps et la vitesse
+_signal = data[1] - np.average(data[1]) #watt                          # signal, puissance
 
 fourier = np.fft.fft(_signal)                                       # fft du signal
 freq = np.fft.fftfreq(len(position), d=position[1]-position[0])     # on génère une matrice des fréquences dans l'espace de Fourier, en 1/dx (unités),  donc m^-1
@@ -27,7 +27,7 @@ lo_pos =  freq[:int(len(freq)/2)]                              # si on inverse (
 fourier_pos = abs(fourier[:int(len(fourier)/2)])
 
 #échantillon
-position_e = r'C:\Users\Sandrine Poulin\OneDrive\Documents\Hiver_2021\Labs TPOP\spectroscopie-ir\Tests_13-04-2021\S2.csv'
+position_e = r'C:\Users\Sandrine Poulin\OneDrive\Documents\Hiver_2021\Labs TPOP\spectroscopie-ir\Tests_13-04-2021\Jack_240s.csv'
 data_e = pd.read_csv(position_e)                # lecture du csv
 data_e = np.array(data_e).transpose()           # conversion du data en array
 
@@ -36,7 +36,7 @@ data_e = np.array(data_e).transpose()           # conversion du data en array
 ## Vitesse de déplacement = 0.1micron/80ms, donc on peut connaître le déplacement total lors de l'acquisition
 
 vitesse_e = 0.1e-6/80e-3
-distance_e = 120 * vitesse_e
+distance_e = 240 * vitesse_e
 
 temps_e = data_e[0]                                                     # temps
 position_e = np.linspace(0, distance_e, len(temps_e))                     # matrice position généré avec le temps et la vitesse
@@ -52,8 +52,9 @@ fourier_pos_e = abs(fourier_e[:int(len(fourier_e)/2)])
 #Spectr d'absorption
 fourier_pos_t = fourier_pos[:200] / fourier_pos_e[:200]
 
-#plt.plot(freq[:int(len(freq)/2)]*3e8*1e6, abs(fourier[:int(len(fourier)/2)]))         #on plot seulement la deuxième moitié, pas les fréquences négatives.
-plt.plot(lo_pos[:200], fourier_pos_t, label = 'A1', color= 'black')
-plt.xlabel("nombre d'onde (1/m)")
-plt.savefig(r'C:\Users\Sandrine Poulin\OneDrive\Documents\Hiver_2021\Labs TPOP\spectroscopie-ir\Analyse (png)\S2_spectre.png', dpi = 600)
+#plt.plot(freq[:int(len(freq)/2)]*3e8*1e6, abs(fourier[:int(len(fourier)/2)]))
+plt.plot(lo_pos[:200] / 100, fourier_pos_t[:200], label = 'A1', color= 'black')
+plt.xlim(0,6000)
+plt.xlabel("nombre d'onde (1/cm)")
+#plt.savefig(r'C:\Users\Sandrine Poulin\OneDrive\Documents\Hiver_2021\Labs TPOP\spectroscopie-ir\Analyse (png)\S2_spectre.png', dpi = 600)
 plt.show()
